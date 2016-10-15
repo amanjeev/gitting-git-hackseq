2bec5cb9ae7d49554e22ca67ea1b5a95b5c2b987
Tommy Carstensen <tc9@sanger.ac.uk>
2016-10-15 23:01:32 +0100

    Unicorn is supercalifragilisticexpialidocious...

--- hack_all_the_seqs.py
+++ hack_all_the_seqs.py
@@ -3,7 +3,28 @@ import requests
 
 def hack_what(thing_to_hack=None):
     if not thing_to_hack:
-        thing_to_hack = "Hack all the Seqs!"
+        thing_to_hack = '''                              /
+                   __       //
+                   -\= \=\ //
+                 --=_\=---//=--
+               -_==/  \/ //\/--
+                ==/   /O   O\==--
+   _ _ _ _     /_/    \  ]  /--
+  /\ ( (- \    /       ] ] ]==-
+ (\ _\_\_\-\__/     \  (,_,)--
+(\_/                 \     \-
+\/      /       (   ( \  ] /)
+/      (         \   \_ \./ )
+(       \         \      )  \
+(       /\_ _ _ _ /---/ /\_  \
+ \     / \     / ____/ /   \  \
+  (   /   )   / /  /__ )   (  )
+  (  )   / __/ '---`       / /
+  \  /   \ \             _/ /
+  ] ]     )_\_         /__\/
+  /_\     ]___\
+ (___)'''
     return thing_to_hack
 
-
+if __name__ == '__main__':
+    print(hack_what())
