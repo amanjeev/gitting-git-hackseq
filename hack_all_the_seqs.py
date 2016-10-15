c8750d303dc7084e2fa48615d97fc2c6a40aa5b6
hwl9 <hwl9@sfu.ca>
2016-10-15 15:05:37 -0700

    suuup

--- hack_all_the_seqs.py
+++ hack_all_the_seqs.py
@@ -3,7 +3,7 @@ import requests
 
 def hack_what(thing_to_hack=None):
     if not thing_to_hack:
-        thing_to_hack = "Hack all the Genjis!"
+        thing_to_hack = "Hack all the hanzo!"
     return thing_to_hack
 
 
